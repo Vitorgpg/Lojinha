ae7b12c685ca1a83d1adb3b173192c83ccc84f11
Vitorgpg <141376701+Vitorgpg@users.noreply.github.com>
2023-09-12 22:25:52 -0300

    Mongo

---
+++ LojinhaServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojinhaServer.Models;
+using MongoDB.Driver;
+using LojinhaServer.Repositories;
+
+namespace LojinhaServer.Extensions;
+public static class ServiceExtensions
+{
+public static void ConfigureCors(this IServiceCollection services)
+{
+services.AddCors(options =>
+{
+options.AddPolicy("CorsPolicy",
+builder => builder.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader());
+});
+}
+public static void ConfigureMongoDBSettings(this IServiceCollection services,
+IConfiguration config)
+{
+services.Configure<MongoDBSettings>(
+config.GetSection("MongoDBSettings")
+);
+services.AddSingleton<IMongoDatabase>(options => {
+var settings =
+config.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+var client = new MongoClient(settings.ConnectionString);
+return client.GetDatabase(settings.DatabaseName);
+});
+}
+
+public static void ConfigureProductRepository(this IServiceCollection services)
+{
+services.AddSingleton<IProductRepository, ProductRepository>();
+}
+
+}
